dde8c6d8257314735c952328f3f990e64cb17a5d
Jakub Červený <jakub.cerveny@melown.com>
2017-10-18 11:56:53 +0200

    Added shaders/surface.glsl

---
+++ shaders/surface.glsl
@@ -0,0 +1,22 @@
+
+
+#if _VERTEX_
+
+//uniform mat4 MVP;
+
+void main()
+{
+    //gl_Position = MVP * gl_Vertex;
+    gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex;
+
+    //normal = gl_NormalMatrix * gl_Normal;
+}
+
+#elif _FRAGMENT_
+
+void main()
+{
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+
+#endif
