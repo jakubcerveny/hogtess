3c76b4acd428584bcef46d10150a2fdd5554f167
Jakub Cerveny <jakub.cerveny@gmail.com>
2017-11-18 22:43:32 +0100

    Using MFEM

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
+#include <fstream>
 
 #include <QApplication>
 
 #include "main.hpp"
 #include "render.hpp"
+
+#include "mfem.hpp"
 
 
 MainWindow::MainWindow(QWidget* gl)
@@ -15,10 +17,18 @@ MainWindow::MainWindow(QWidget* gl)
 
 int main(int argc, char *argv[])
 {
-    /*if (argc < 2) {
+    if (argc < 2) {
         std::cout << "Usage: hogtess <mesh> <solution>\n";
         return EXIT_FAILURE;
-    }*/
+    }
+
+    std::cout << "Loading mesh: " << argv[1] << std::endl;
+    mfem::Mesh mesh(argv[1]);
+
+    std::cout << "Loading solution: " << argv[2] << std::endl;
+    std::ifstream is(argv[2]);
+    mfem::GridFunction gridfn(&mesh, is);
+    is.close();
 
     QApplication app(argc, argv);
 
